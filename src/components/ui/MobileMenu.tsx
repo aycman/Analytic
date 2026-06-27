84c76828dc3282e1e551ab4e1eb0f736f3bc07a1
aycman <man.hajilou@gmail.com>
2026-06-27 13:33:51 +0200

    add sub menu on MobileMenu.tsx

--- src/components/ui/MobileMenu.tsx
+++ src/components/ui/MobileMenu.tsx
@@ -1,6 +1,25 @@
+/**
+ * Components
+ */
+import {Button} from '@/components/ui/button';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {Separator} from '@/components/ui/separator';
 
+/**
+ * Assets
+ */
+import { ChevronsUpDown } from 'lucide-react';
+
+/**
+ * Types
+ */
 import type {MenuItem} from '@/types';
 import { navMenu } from '../../constants/index';
+
 
 type MobileMenuProps = {
   navMenu: MenuItem[]
@@ -9,9 +28,70 @@ type MobileMenuProps = {
 const MobileMenu = ({navMenu}: MobileMenuProps) => {
   return (
     <div>
-        <ul className="">
+        <ul className="mb-3">
+          {navMenu.map(({href, label, submenu}, index) => (
+            <li key={index}>
+              {submenu ? (
+                <Collapsible>
+                  <CollapsibleTrigger asChild>
+                    <Button
+                      variant='ghost'
+                      className=' w-full flex justify-between'
+                    >
+                      {label}
+
+                      <ChevronsUpDown />
+                    </Button>
+                  </CollapsibleTrigger>
+
+                  <CollapsibleContent className='ps-2'>
+                    <ul className="border-l border-l-muted-foreground/20">
+                      {submenu.map(({href, label}, index) => (
+                        <li key={index}>
+                          <Button
+                            asChild
+                            variant='ghost'
+                            className='w-full justify-start 
+                            text-muted-foreground 
+                            hover:bg-transparent'
+                          >
+                            <a href={href}>{label}</a>
+                          </Button>
+
+                        </li>
+                      ))}
+                    </ul>
+                  </CollapsibleContent>
+                </Collapsible>
+              ) : (
+                <Button
+                  asChild
+                  variant='ghost'
+                  className=' w-full justify-start'
+                >
+                  <a href={href}>{label}</a>
+                </Button>
+              )}
+            </li>
+          ))}
+
  
         </ul>
+
+        <Separator className='bg-muted-foreground/20' />
+
+        <div className="grid grid-cols-2 gap-2 mt-4">
+          <Button 
+          variant='ghost' 
+          className='w-full'
+          >
+            Sign In
+          </Button>
+
+          <Button className='w-full'>
+            Free Trial
+          </Button>
+        </div>
     </div>
   );
 }
